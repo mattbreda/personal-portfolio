9ae40353eec91f486a7750918bb73780c5feadcb
mattbreda <mattbreda@gmail.com>
2024-03-29 10:58:19 +0100

    coockiebot

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,17 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import { IconList, IconMessage, IconUser } from "@tabler/icons-react";
 import { Toaster } from "react-hot-toast";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Script from "next/script";
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GTAG || "";
 
 export const metadata: Metadata = {
   title: "Matteo Breda | Developer",
   description:
-  "Welcome to the website of Matteo Breda, freelance javascript developer!",
+    "Welcome to the website of Matteo Breda, freelance javascript developer!",
 };
 
 const rMono = Roboto_Mono({ subsets: ["latin"] });
@@ -48,6 +49,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${rMono.className} bg-neutral-100`}>
+        <Script
+          id="Cookiebot"
+          strategy="afterInteractive"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="a4b1dc6b-2a65-48ca-a446-33a06e084d7f"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></Script>
         <Toaster />
         <main className="w-full relative">
           <FloatingNav navItems={navItems} />
