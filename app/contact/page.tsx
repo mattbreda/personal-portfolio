ea6850c128cfc16533a45ee2942280128d404504
mattbreda <mattbreda@gmail.com>
2024-03-29 11:10:03 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <main className="flex md:h-screen min-h-screen flex-col items-center justify-between max-md:pb-2.5 pt-2.5 md:pt-10 w-full">
       <div className="max-w-[1400px] mx-auto w-full h-full ">
-        <div className="w-full h-full px-2.5 ">
+        <div className="w-full h-full px-2.5 flex flex-col">
           <TextGenerateEffect
             words="Contacts"
             className="text-3xl md:text-5xl text-green-500"
